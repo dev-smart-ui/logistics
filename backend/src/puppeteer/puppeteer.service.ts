f8903dac319776211ffb6db6da5932eb2b29119d
Koval Vlad <koval.vladuslav.v@gmail.com>
2024-10-22 15:45:13 +0300

    launch puppeteer

--- backend/src/puppeteer/puppeteer.service.ts
+++ backend/src/puppeteer/puppeteer.service.ts
@@ -50,7 +50,10 @@ export class PuppeteerService {
 
   async scrapeData(url: string, selector: string): Promise<any> {
     try {
-      const browser = await puppeteer.launch();
+      const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      });
+
       const page = await browser.newPage();
   
       await page.goto(url, { waitUntil: 'networkidle0' });
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,8 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*', // путь на фронтенде
-        destination: 'http://185.233.38.15/logistic/backend/:path*' // адрес бэкенда без SSL
+        destination: 'http://185.233.38.15:3100/:path*'
+        // destination: 'http://185.233.38.15/logistic/backend/:path*' // адрес бэкенда без SSL
         // destination: 'http://localhost:3100/:path*', // Зміна на localhost для локальної розробки
         // destination: 'http://183.55.66.88/:path*',
       },
